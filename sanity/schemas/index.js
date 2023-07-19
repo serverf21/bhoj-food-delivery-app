cb54f7988a9bb91a3a060c876d675d6bc719972f
Sarvagya Saxena <sarvagyasaxena.2102@gmail.com>
2023-07-20 04:39:45 +0530

    Complete setup for sanity backend, base UI for homepage, next step to extract data and feed into the UI

---
+++ sanity/schemas/index.js
@@ -0,0 +1,7 @@
+
+import category from './category'
+import restraunt from './restraunt'
+import dish from './dish'
+import featured from './featured'
+
+export const schemaTypes = [restraunt, dish, category, featured]
